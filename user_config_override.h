485b48805f5c40d1d1134c91595058e8eb16e82e
krishy2 <75061368+krishy2@users.noreply.github.com>
2025-03-13 03:00:47 +0100

    Create user_config_override.h
    Tasmota compile options

---
+++ user_config_override.h
@@ -0,0 +1,19 @@
+#ifndef USE_SCRIPT
+#define USE_SCRIPT
+#endif
+#ifndef USE_SML_M
+#define USE_SML_M
+#endif
+#ifdef USE_RULES
+#undef USE_RULES
+#endif
+
+#define SML_BSIZ 1060
+#define MAX_METERS 1
+#define TMSBSIZ 2048
+#define USE_SML_SCRIPT_CMD
+#define SML_REPLACE_VARS
+
+#define USE_SCRIPT_JSON_EXPORT
+
+#define USE_SCRIPT_WEB_DISPLAY
